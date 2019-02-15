05560219554f55b1624287ec401c3aa56081a2a1
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-15 13:52:06 -0500

    updated styles

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -5,7 +5,9 @@ import CharactersList from './CharactersList';
 const StarWars = (props) => {
 	return (
 		<div>
-			<h1>Characters</h1>
+			<div className="title-container">
+				<h1 className="characters-title">Characters</h1>
+			</div>
 			<CharactersList characters={props.characters} clickOnName={props.clickOnName} />
 		</div>
 	);
