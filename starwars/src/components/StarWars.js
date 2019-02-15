164c8cb2147978f9709889d4bb8cf62b6ac32668
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-15 13:33:03 -0500

    added styles and characters header

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -5,6 +5,7 @@ import CharactersList from './CharactersList';
 const StarWars = (props) => {
 	return (
 		<div>
+			<h1>Characters</h1>
 			<CharactersList characters={props.characters} clickOnName={props.clickOnName} />
 		</div>
 	);
