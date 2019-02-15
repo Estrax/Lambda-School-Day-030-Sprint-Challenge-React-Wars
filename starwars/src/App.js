d2e08e3b7402672c5b87350256cb25a55b45797a
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-15 11:27:52 -0500

    changed spaces to tabs in app.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,40 +3,40 @@ import './App.css';
 import StarWars from './components/StarWars';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      starwarsChars: []
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			starwarsChars: []
+		};
+	}
 
-  componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people');
-  }
+	componentDidMount() {
+		this.getCharacters('https://swapi.co/api/people');
+	}
 
-  getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
-    fetch(URL)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
-  };
+	getCharacters = URL => {
+		// feel free to research what this code is doing.
+		// At a high level we are calling an API to fetch some starwars data from the open web.
+		// We then take that data and resolve it our state.
+		fetch(URL)
+			.then(res => {
+				return res.json();
+			})
+			.then(data => {
+				this.setState({ starwarsChars: data.results });
+			})
+			.catch(err => {
+				throw new Error(err);
+			});
+	};
 
-  render() {
-    return (
-      <div className="App">
-        <StarWars />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<StarWars />
+			</div>
+		);
+	}
 }
 
 export default App;
